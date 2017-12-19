97874e93bc08a90e57700bce3a7f6c44943430ea
helkinchacon <chacon2016251013@unitropico.edu.co>
2017-12-19 00:27:21 -0500

    First Up
    In this proyect version the logic part is the priority.
    The design is in work.

---
+++ Bingo/script.js
@@ -0,0 +1,135 @@
+function myFunction() {
+	var i = 0
+	for(i=0;i<25;i++){
+		switch(i){
+			case 0:
+	    			document.getElementById("demo").innerHTML =  Math.floor((Math.random() * 100));
+			break;
+			case 1:
+	   				document.getElementById("demo2").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 2:
+	    			document.getElementById("demo3").innerHTML = Math.floor((Math.random() * 100));
+			break;
+	    	case 3 :
+	    			document.getElementById("demo4").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 4:
+	    			document.getElementById("demo5").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 5:
+	    			document.getElementById("demo6").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 6:
+	    			document.getElementById("demo7").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 7:
+	    			document.getElementById("demo8").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 8:
+					document.getElementById("demo9").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 9:
+					document.getElementById("demo10").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 10:
+					document.getElementById("demo11").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 11:
+					document.getElementById("demo12").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 12:
+					document.getElementById("demo13").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 13:
+					document.getElementById("demo14").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 14:
+					document.getElementById("demo15").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 15:
+	    			document.getElementById("demo16").innerHTML =  Math.floor((Math.random() * 100));
+			break;
+			case 16:
+	   				document.getElementById("demo17").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 17:
+	    			document.getElementById("demo18").innerHTML = Math.floor((Math.random() * 100));
+			break;
+	    	case 18:
+	    			document.getElementById("demo19").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 19:
+	    			document.getElementById("demo20").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 20:
+	    			document.getElementById("demo21").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 21:
+	    			document.getElementById("demo22").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 22:
+	    			document.getElementById("demo23").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 23:
+					document.getElementById("demo24").innerHTML = Math.floor((Math.random() * 100));
+			break;
+			case 24:
+					document.getElementById("demo25").innerHTML = Math.floor((Math.random() * 100));
+			break;
+		}
+	}
+}
+var int=self.setInterval("recargar",60000);
+    function recargar()
+		    {
+        location.reload(true);
+    }
+
+
+var num = [0,1,2,3,4,5,6,7,8];
+  
+ function lax()
+{
+
+  
+
+
+var indice = Math.floor(Math.random()*num.length);
+var i = num[indice];
+num.splice(indice, 1);
+ 
+		switch(i){
+			case 0:
+	    			document.getElementById("demo").innerHTML =  Math.floor((Math.random() * 100));
+
+			break;
+			case 1:
+	   				document.getElementById("demo7").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 2:
+	    			document.getElementById("demo13").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 3:
+	    			document.getElementById("demo19").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 4:
+	    			document.getElementById("demo25").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 5:
+	    			document.getElementById("demo5").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 6:
+	    			document.getElementById("demo9").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 7:
+	    			document.getElementById("demo17").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	case 8:
+	    			document.getElementById("demo21").innerHTML = Math.floor((Math.random() * 100));
+	    	break;
+	    	
+   
+
+}
+}
